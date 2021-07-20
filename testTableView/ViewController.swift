9c8c694e385960f8649b6982b26f9058550786d9
sungh7 <50727298+sungh7@users.noreply.github.com>
2021-07-20 15:23:07 +0900

    Initial Commit

---
+++ testTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTableView
+//
+//  Created by 최성훈 on 2021/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
